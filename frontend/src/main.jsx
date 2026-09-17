35758c6f72e50009bdacb1d469a3202369aec69b
Phongsakorn <phongsakorn.ka@ksu.ac.th>
2026-09-17 13:31:57 +0700

    Auth: Embed Google Client ID in build and render config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,9 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App.jsx";
 import "./index.css";
 
-const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || "sample-google-client-id.apps.googleusercontent.com";
+const googleClientId =
+  import.meta.env.VITE_GOOGLE_CLIENT_ID ||
+  "231790850035-0f63f0dpqkeng3mh9783bg8ksposp3hg.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
